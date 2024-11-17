b7981c3fc9d8d9a9b222e6ac8e216e5a364cb68c
DevTec2020 <leonardo.silva.inf@gmail.com>
2024-11-16 21:55:44 -0300

    codando data e hora do agendamento

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,7 +1,12 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
 
-export function hoursLoad(){
+const hours = document.getElementById("hours")
+
+export function hoursLoad({ date }){
+    // Limpa a lista de horarios
+    hours.innerHTML = ""
     const opening = openingHours.map((hour) => {
         // Recupera somente a hora.
         const [scheduleHour] = hour.split(":")
@@ -11,8 +16,41 @@ export function hoursLoad(){
         
         return ({
             hour,
-            available: !isHourPast,
+            available: isHourPast,
         })
 
     })
+
+    // Renderiza os horários.
+    opening.forEach(({hour, available}) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+
+        // Verifica período 
+        if(hour === "9:00"){
+            hourHeaderAdd("Manhã")
+        } else if (hour === "13:00"){
+            hourHeaderAdd("Tarde")
+        } else if (hour === "18:00"){
+            hourHeaderAdd("Noite")
+        }
+
+        hours.append(li)
+    })
+
+    // Adiciona o foco no horario clicado
+    hoursClick()
+}
+
+// Adciona divisão de manhã tarde ou noite
+function hourHeaderAdd(title){
+    const header = document.createElement("li")
+    header.classList.add("hour-period")
+    header.textContent = title
+
+    hours.append(header)
 }
